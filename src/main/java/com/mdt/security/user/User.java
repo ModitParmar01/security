06f0e0c0cd76397f6a6277318c3dc673cd6f0cb7
Modit Parmar <modit987@gmail.com>
2023-09-08 10:52:43 +0530

    JAVA authentication.

---
+++ src/main/java/com/mdt/security/user/User.java
@@ -0,0 +1,69 @@
+package com.mdt.security.user;
+
+import org.hibernate.mapping.Collection;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+
+
+
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
+    @Override
+    public java.util.Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+}
